2a96fb121b6abe6a543cc8e8a3bf73d25d4853af
vidhyasagar jallipalli <vidhyasagar.jallipalli@oracle.com>
2024-06-22 15:19:57 +0530

    done

---
+++ template.py
@@ -0,0 +1,7 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)]:%(message)s')
+
+logging.info("Hello Everyone. welcome to this course.")
